3afb3c53cb557f47d39976caa1ff0acb0d1ea17b
eduardomarini <eduardoribeiromarini@gmail.com>
2024-02-21 22:39:13 -0300

    Definição dos dados das pedras preciosas

---
+++ pedras_preciosas_FB.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+// estrutura que contém os dados das pedras preciosas
+struct pedrasPreciosas {
+    string nome;
+    float volume;
+    int peso;
+    int preco;
+};
+
+pedrasPreciosas dados[] = {
+    {"Diamante", 0.455, 263, 500},
+    {"Esmeralda", 0.521, 127,410},
+    {"Topazio", 0.857, 254, 320},
+    {"Rubi", 0.065, 134, 315},
+    {"Jade", 0.012, 111, 280}
+};
